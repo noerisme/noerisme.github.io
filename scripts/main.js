0c26e56d5d7ee21959d3733deb9ba95c8d32ec01
Muhammad Nur Azhar <63506653+noerisme@users.noreply.github.com>
2021-01-21 13:39:41 +0700

    trying to fix 2x audio in background

--- scripts/main.js
+++ scripts/main.js
@@ -216,8 +216,9 @@
   else {
       $('#playAudio').remove() // just to make sure that it will not have 2x audio in the background 
   }
-  // setTimeout(() => {
+  
+  setTimeout(() => {
     init()  
-  // }, 4000);
-  // mp3.play()
+  }, 4000);
+  mp3.play()
 })(window)
